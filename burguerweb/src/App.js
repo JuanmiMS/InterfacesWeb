608fc83055e4bd5b316e97133a319d6ffbd02e85
Juan Miguel <juanmi.marisanchez@gmail.com>
2019-02-06 19:21:23 +0100

    refactorizado

--- burguerweb/src/App.js
+++ burguerweb/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomeHOC from './containers/Home/home';
 import HeaderHOC from './containers/Header/header';
